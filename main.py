ffe07ad6859109aa21a3fccfc1b8f08f9c5cf669
alpha-k911 <nandishwar.k911@gmail.com>
2019-06-16 23:30:32 +0530

    added time functionality

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import speech_recognition as sr
 import pyaudio
 import os
+import time
+
+# from sqlalchemy import true
+
 r = sr.Recognizer()
 
 #with noalsaerr() as n, Microphone() as source:
 with sr.Microphone() as source:
-    print("Say something that i can recognize: ")
-    audio = r.listen(source)
-    try:
-        print("[+] Listening...."+"\n")
-        text = r.recognize_google(audio)
-        print("I guess {}".format(text))
-        if text=="Alpha":
-            os.system("gnome-screensaver-command -l")
-    except:
-        print("I didn't get")
-
+    while True:
+        time.sleep(2)
+        print("Say something that i can recognize: ")
+        audio = r.listen(source)
+        try:
+            print("[+] Listening...."+"\n")
+            text = r.recognize_google(audio)
+            print("I guess {}".format(text))
+            if text.__contains__("Alpha"):
+                os.system("gnome-screensaver-command -l")
+        except:
+            print("I didn't get")
